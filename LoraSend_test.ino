dce7b9db09a4b72c32336f22f4608ec22ee2be92
SenneDeWinter <71653655+SenneDeWinter@users.noreply.github.com>
2021-06-01 11:39:44 +0200

    Added LoraSend_test

---
+++ LoraSend_test.ino
@@ -0,0 +1,62 @@
+#include <MKRWAN.h>
+
+LoRaModem modem;
+
+
+#include "arduino_secrets.h"
+// Please enter your sensitive data in the Secret tab or arduino_secrets.h
+String appEui = SECRET_APP_EUI;
+String appKey = SECRET_APP_KEY;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial);
+  // change this to your regional band (eg. US915, AS923, ...)
+  if (!modem.begin(EU868)) {
+    Serial.println("Failed to start module");
+    while (1) {}
+  };
+  Serial.print("Your module version is: ");
+  Serial.println(modem.version());
+  Serial.print("Your device EUI is: ");
+  Serial.println(modem.deviceEUI());
+
+  int connected = modem.joinOTAA(appEui, appKey);
+  if (!connected) {
+    Serial.println("Something went wrong; are you indoor? Move near a window and retry");
+    while (1) {}
+  }
+
+  // Set poll interval to 60 secs.
+  modem.minPollInterval(60);
+  // NOTE: independent of this setting, the modem will
+  // not allow sending more than one message every 2 minutes,
+  // this is enforced by firmware and can not be changed.
+}
+
+void loop() {
+
+  while (!Serial.available());
+  String msg = waterstring;
+
+  Serial.println();
+  Serial.print("Sending: " + msg + " - ");
+  for (unsigned int i = 0; i < msg.length(); i++) {
+    Serial.print(msg[i] >> 4, HEX);
+    Serial.print(msg[i] & 0xF, HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  int err;
+  modem.beginPacket();
+  modem.print(msg);
+  err = modem.endPacket(true);
+  if (err > 0) {
+    Serial.println("Message sent correctly!");
+  } else {
+    Serial.println("Error sending message :(");
+    Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
+    Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
+  }
